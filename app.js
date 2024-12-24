71f00ca075a7198dd4891d8d11f354e1584a23a5
Muzammil Usman <153772378+muzammil-usman@users.noreply.github.com>
2024-12-25 01:31:52 +0500

    may be form correct (#4) (#5)
    Co-authored-by: MuhammadUmaim <muhammadumaim7@gmail.com>

--- app.js
+++ app.js
@@ -32,6 +32,45 @@ else{
 }
 
 
+let emailValid = function(e) {
+  var error = e.target.nextElementSibling;
+  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailPattern.test(e.target.value)) {
+    error.innerText = 'Enter a valid email';
+    error.style.display = "block";
+    error.style.color = "#00796b";
+    disableStartButton();
+  } else {
+    error.style.display = "none";
+    enableStartButton();
+  }
+};
+
+
+
+
+
+let startCounT = (event) => {
+  event.preventDefault();
+
+  var name = document.getElementById('inpName').value;
+  var roll = document.getElementById('inpRoll').value;
+  var email = document.getElementById('inpEmail').value;
+
+  // Check if all fields are filled
+  if (name === "" || roll === "" || email === "") {
+    alert("Please fill in all the fields.");
+  } else {
+    window.location.href = "./quiz.html";
+  }
+};
+
+
+
+
+
+
+
 let GoToQuiz = () =>{
 
    window.location.href = "./quiz.html";
